35cb533fc1f42bf29ec92adb5709d5ac65ac2c62
Brooke Riggio <brookr@pridwen.local>
2008-09-23 17:06:14 -0700

    added jquery, started events model

---
+++ db/migrate/002_create_events.rb
@@ -0,0 +1,27 @@
+class CreateEvents < ActiveRecord::Migration
+  def self.up
+    create_table :events do |t|
+      
+      t.string    :type,            :limit => 40 #allows polymorphism
+      
+      t.string    :creator,         :limit => 40
+      t.string    :summary,         :limit => 140
+      t.string    :status,          :limit => 40
+      t.string    :location,        :limit => 140
+      t.string    :recur
+      t.string    :timezone         
+      
+      t.text      :description      
+      
+      t.datetime  :start
+      t.datetime  :end
+      
+      
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :events
+  end
+end
